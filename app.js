b6ab80845e1b90df1e2c5c473ab15aeec46c06b4
Aulero99 <auston.robertson@gmail.com>
2023-03-23 13:53:14 -0600

    known issue: timer malfunctioning

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let startButton = document.getElementById("start-button");
 let inflateButton = document.getElementById("inflate-button");
+let clickCountElem = document.getElementById("click-count");
 
 let clickCount = 0;
 let balloonHeight = 120;
@@ -7,34 +8,45 @@ let balloonWidth = 100;
 let inflationRate = 20;
 let maxSize = 300;
 let popCount = 0;
-let timer = 2000;
+let gameLength = 4000;
+let timeRemain = 0;
+let clockId = 0;
 
 
 
 function startGame(){
     startButton.setAttribute("disabled", "true");
     inflateButton.removeAttribute("disabled");
-    setTimeout(() => {
-        inflateButton.setAttribute("disabled", "true");
-        startButton.removeAttribute("disabled");
+    startClock();
+    setTimeout(stopGame, gameLength);
+}
 
-        console.log(timer);
-    }, timer);
+function startClock(){
+    timeRemain = gameLength;
+    drawClock();
+    clockId = setInterval(drawClock, 1000);
+}
+
+function stopClock(){
+    clearInterval(clockId);
+}
+
+function drawClock() {
+    let countdown = document.getElementById("countdown");
+    countdown.innerText = (timeRemain/1000).toString();
+    timeRemain -= 1000;
+    console.log(timeRemain);
 }
 
 function inflate(){
-    let clickCountElem = document.getElementById("click-count");
-
     clickCount++;
-    clickCountElem.innerText = clickCount.toString();
     balloonHeight += inflationRate;
     balloonWidth += inflationRate;
     draw();
-
+    
     if (balloonHeight >= maxSize){
-        console.log("Pop the Balloon");
         popCount++;
-        document.getElementById("pop-count").innerText = popCount.toString();
+        console.log("Popped " + popCount);
         balloonHeight = 10;
         balloonWidth = 10;
         draw();
@@ -48,4 +60,22 @@ function draw() {
     balloonElement.style.height = balloonHeight + "px";
     balloonElement.style.width = balloonWidth + "px";
     
-}
+    document.getElementById("pop-count").innerText = popCount.toString();
+    clickCountElem.innerText = clickCount.toString();
+};
+
+function stopGame() {
+        stopClock();
+        console.log ("game over");
+        inflateButton.setAttribute("disabled", "true");
+        startButton.removeAttribute("disabled");
+
+        console.log("Clicked " + clickCount + " times.");
+        console.log("Popped " + popCount + " baloons.");
+        clickCount = 0;
+        popCount = 0;
+        balloonHeight = 120;
+        balloonWidth = 100;
+
+        draw();
+};
